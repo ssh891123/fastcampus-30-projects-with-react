fd4820136ec17b9477fee25b6e45a4620d68288a
ssh891123 <ssh891123@gmail.com>
2023-07-26 23:13:52 +0900

    [feat] draggable 컴포넌트 적용
    - memo component을 draggable 컴포넌트로 wrap
    - node-sass 업데이트: node version을 업데이트 하면서 기존 버젼은 사용할 수 없음

--- sticker-memo/src/Memo/Memo.jsx
+++ sticker-memo/src/Memo/Memo.jsx
@@ -1,27 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import DragHandleIcon from "@mui/icons-material/DragHandle";
 import CloseIcon from "@mui/icons-material/Close";
 import "./Memo.scss";
+import Draggable from '@ssh891123/draggable';
     
   function Memo({ item, Delete, Edit, SetPosition, SetWidthHeight }) {
+    const handleRef = useRef(null);
+
     return (
-        <div
-          className="memo-container"
-          style={{ width: `${250}px`, height: `${300}px` }}
-        >
-            <div className="menu">
-                <DragHandleIcon sx={{ cursor: "move", fontSize: "25px" }} />
-                <CloseIcon 
-                    sx={{ cursor: "pointer", fontSize: "25px", float: "right" }}
-                />
+        <Draggable handleRef={handleRef} x={0} y={0} onMove={(x, y) => console.log(x, y)}>
+            <div
+                className="memo-container"
+                style={{ width: `${250}px`, height: `${300}px` }}
+            >
+                <div className="menu">
+                    <DragHandleIcon 
+                        ref={handleRef}
+                        sx={{ cursor: "move", fontSize: "25px" }} />
+                    <CloseIcon
+                        sx={{ cursor: "pointer", fontSize: "25px", float: "right" }}
+                    />
+                </div>
+                <textarea
+                    className="memo-text-area"
+                    defaultValue={"Enter memo here"}
+                    name="txt"
+                    placeholder="Enter memo here"
+                ></textarea>
             </div>
-            <textarea
-                className="memo-text-area"
-                defaultValue={"Enter memo here"}
-                name="txt"
-                placeholder="Enter memo here"
-            ></textarea>
-        </div>
+        </Draggable>
     );
   }
   
