b00e24b2c0a856f263a10b030869ceb0a5fb72aa
ssh891123 <ssh891123@gmail.com>
2023-07-27 23:41:16 +0900

    [feat] 메모 추가 버튼 기능 구현
    - 메모를 추가할 수 있는 버튼 기능 구현
    - mobx로 메모의 state를 observer 되도록 구현

--- sticker-memo/src/App.js
+++ sticker-memo/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { observer } from 'mobx-react';
+import AddIcon from '@mui/icons-material/Add';
 import Memo from './Memo/Memo';
 
-function App() {
+function App({store}) {
+  const AddMemo = useCallback(() => store.addMemo(), [store]);
+
   return (
-    <Memo/>
+    <> {
+      store.memos.map((item) => <Memo key={item.id} />)
+    }
+    <AddIcon 
+      sx={{
+        float:"right", 
+        backgroundColor: "#e4e4e4", 
+        borderRadius:"5px", 
+        cursor:"pointer", 
+        fontSize:"30px",
+        border: "1px solid black"
+      }}
+      onClick={AddMemo}/>
+    </>
   );
 }
 
-export default App;
+export default observer(App);
