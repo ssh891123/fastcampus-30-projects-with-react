317a9c489c4f7d37d0cf4623e58c8fbe7cfea043
ssh891123 <ssh891123@gmail.com>
2023-07-31 20:20:14 +0900

    [feat] 명언 response와 추가하는 기능
    - get을 호출하면 명언을 반환
    - post을 호출하면 명언 목록에 추가

--- ch9/README.md
+++ ch9/README.md
@@ -3,4 +3,8 @@
 Nodemon: 서버 코드를 변경하면 자동으로 반영해주는 library
 
 $ npx nodemon index.js
-//nodemon을 설치하지 않고도 Nodemon처럼 도앚ㄱ
+//nodemon을 설치하지 않고도 Nodemon처럼 동작
+
+postman 설치 후 test가능
+
+명언 출처: https://github.com/golbin/hubot-maxim
--- ch9/index.js
+++ ch9/index.js
@@ -1,18 +1,47 @@
 //import express from 'express; 동일
 const express = require('express'); 
+const fs = require('fs');
+
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+// console.log(data);
 
 const app = express();
+app.use(express.json());
 
 //[req]request - client가 server에 요청줄때
 //[res]response - 어떻게 응답할지
 // data를 가져올 때 사용
 app.get('/', (req, res) => {
-    res.send("<h1>I'm get methode!!</h1>");
+    // res.send("<h1>I'm get methode!!</h1>");
+    res.json(data);
 });
 
 // data를 생성할 때 사용(예: user생성)
 app.post('/', (req, res) => {
-    res.send("<h1>I'm post methode</h1>");
+    console.log(req.body);
+    
+    const { author, message } = req.body;
+    if(!(author && author.length > 0 &&
+        message && message.length> 0)) {
+        //응답: json에 추가되지 않음
+        res.json({
+            rs:false
+        });
+        return;
+    }
+
+    // 추가된 명언은 서버를 껏다 켜면 제거됨
+    data.push({
+        author: req.body.author,
+        message: req.body.message
+    });
+
+    //응답: json에 추가
+    res.json({
+        rs: true
+    })
+
+    // res.send("<h1>I'm post methode</h1>");
 });
 
 
