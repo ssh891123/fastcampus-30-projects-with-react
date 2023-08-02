da9dc73bcded45334311429bfc25982a24a35101
ssh891123 <ssh891123@gmail.com>
2023-08-02 23:40:59 +0900

    [feat] 서버로 명언 가져오기
    - axios api로 명언 가져오기
    - 메인, 편집 모드로 구분하고 메인에서 명언보여주도록 설정

--- random-wise-saying/README.md
+++ random-wise-saying/README.md
@@ -1,5 +1,8 @@
 랜덤 명언 생성기 - client
 
+postman
+http 요청을 날리고 응답을 보여주는 서비스
+
 styled-system
 styled를 고도화할 수 있게 해줌(예: prop을 통해 style 설정 가능)
 
--- random-wise-saying/src/App.tsx
+++ random-wise-saying/src/App.tsx
@@ -1,45 +1,114 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Flex from './components/Flex';
 import Box from './components/Box';
-import { VscListUnordered } from 'react-icons/vsc';
+import { VscAdd, VscArrowLeft, VscEdit, VscListUnordered, VscTrash } from 'react-icons/vsc';
+import axios from 'axios';
+import Data from './interface/Data';
 
 function App() {
-  return (
-    <>
-      <Flex position={"fixed"} right="64px" top={"64px"}>
-        <Flex 
-          bg="#2699FB" width={"48px"} top={"48px"} borderRadius={"4px"}
-          alignItems="center" justifyContent={"center"}
-        >
-          <VscListUnordered color='white' fontSize={"32px"}/>
-        </Flex>
-      </Flex>
-      <Flex 
-        flexDirection={"column"} 
-        alignItems="center" 
-        justifyContent="center"
-        height={"100vh"}
-        px={"16px"}
-      >
-        <Box fontSize={"24px"}>
-          오늘의 명언
-        </Box>
-        <Flex
-          mt="64px"
-          mb="16px"
-          border={"1px solid #707070"}
-          alignItems={"center"} justifyContent={"center"}
-          width={"100%"} 
-          height={"160px"} fontSize={"48px"}
-        >
-          웃으면 복이 옵니다~~~
-        </Flex>
-        <Box fontSize={"24px"}>
-          react study
-        </Box>
+  const [page, setPage] = useState<'main' | 'edit'>('main');
+  const [nowData, setNowData] = useState<null | Data>(null);
+  const [error, setError] = useState('');
 
+  useEffect(()=>{
+    if(page === 'main') {
+      //CORS Error 발생함 - BackEnd에서 컨트롤 필요
+      axios.get("http://localhost:3000/random").then(
+        e=> setNowData(e.data)
+      ).catch(() => setError('명언을 불러오지 못했습니다')); 
+    }
+
+  }, [page]);
+
+  if (page === 'main') {
+    return (
+      <>
+        <Flex position={"fixed"} right="64px" top={"64px"}>
+          <Flex 
+            bg="#2699FB" width={"48px"} height={"48px"} borderRadius={"4px"}
+            alignItems="center" justifyContent={"center"}
+            onClick={() => setPage('edit')}
+          >
+            <VscListUnordered color='white' fontSize={"32px"}/>
+          </Flex>
+        </Flex>
+        <Flex 
+          flexDirection={"column"} 
+          alignItems="center" 
+          justifyContent="center"
+          height={"100vh"}
+          px={"16px"}
+        >
+          <Box fontSize={"24px"}>
+            오늘의 명언
+          </Box>
+          <Flex
+            style = {{
+              whiteSpace:"nowrap"
+            }}
+            px="32px"
+            overflowX={"scroll"}
+            mt="64px"
+            mb="16px"
+            border={"1px solid #707070"}
+            alignItems={"center"} justifyContent={"center"} maxWidth={"1000px"}
+            width={"100%"} 
+            height={"160px"} fontSize={"48px"}
+          >
+            {error.length > 0 && error}
+            {nowData?.message}
+          </Flex>
+          <Box fontSize={"24px"}>
+            {nowData?.author}
+          </Box>
+
+        </Flex>
+      </>
+    );
+  }
+  return (
+    <Flex pt="64px" pl="64px" flexDirection={"column"}>
+      <Flex 
+        pb="44px"
+        style={{
+          gap:"44px"
+        }}>
+        <Flex 
+          bg="#2699FB" width={"48px"} height={"48px"} borderRadius={"4px"}
+          alignItems="center" justifyContent={"center"}
+          onClick={() => setPage('main')}
+        >
+          <VscArrowLeft color='white' fontSize={"32px"}/>
+        </Flex>
+        <Flex 
+          bg="#2699FB" width={"48px"} height={"48px"} borderRadius={"4px"}
+          alignItems="center" justifyContent={"center"}
+          onClick={() => setPage('main')}
+        >
+          <VscAdd color='white' fontSize={"32px"}/>
+        </Flex>
       </Flex>
-    </>
+
+      <Flex width="416px" height="48px">
+        <Flex border={"solid 1px #707070"} flex ={1}>
+        </Flex>
+        <Flex 
+          bg="#2699FB" width={"48px"} height={"48px"} borderRadius={"4px"}
+          alignItems="center" justifyContent={"center"}
+          onClick={() => setPage('main')}
+        >
+          <VscEdit color='white' fontSize={"32px"}/>
+        </Flex>
+        <Flex
+          bg="#FF0C0C" width={"48px"} height={"48px"} borderRadius={"4px"}
+          alignItems="center" justifyContent={"center"}
+          onClick={() => setPage('main')}
+        >
+          <VscTrash color='white' fontSize={"32px"}/>
+        </Flex>
+        
+      </Flex>
+    </Flex>
   );
 }
 
